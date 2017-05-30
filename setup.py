8cda3e963f7bdf6ca656a26d3e89976303df14b6
Maciej Nowakowski <maciej.nowakowski@allegrogroup.com>
2017-05-30 12:05:30 +0200

    Bump Django in setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     classifiers=CLASSIFIERS,
     install_requires=[
         'setuptools',
-        'Django>=1.8,<1.10',
+        'Django>=1.8,<=1.11',
     ],
     test_suite='django_admin_bootstrapped.runtests.runtests',
     packages=find_packages(),
